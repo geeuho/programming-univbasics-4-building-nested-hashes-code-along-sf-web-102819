b3362f7a71d656f2e32f3f1b0977b8ffc5975a52
Allen Shin <geeuho@gmail.com>
2019-10-22 06:48:52 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,35 +1,35 @@
 def third_challenge
   epic_tragedy = {
   :montague => {
-      :patriarch => {
+      :patriarch => 
         {:name => "Lord Montague",
         :age => "53"}
-        },
-      :matriarch => {
+        ,
+      :matriarch => 
         {:name =>"Lady Montague",
         :age => "54"}
-        },
-      :hero => {
+        ,
+      :hero => 
         {:name => "Romeo",
         :age => "15",
         :status => "alive"}
-        },
+        ,
       :hero_friends => []
   }, 
   :capulet => {
-      :patriarch => {
+      :patriarch => 
         {:name => "Lord Capulet",
         :age => "50"}
-        },
-      :matriarch => {
+        ,
+      :matriarch => 
         {:name => "Lady Capulet",
         :age => "51"}
-        },
-      :heroine => {
+        ,
+      :heroine => 
         {:name => "Juliet",
         :age => "15",
         :status => "alive"}
-        },
+        ,
       :heroine_friends => []
   }
   }
